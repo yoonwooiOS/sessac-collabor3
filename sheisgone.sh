fd679dd0a9143fdbcda17c14ddc5dc403c5f189e
jimmy <71679088+wjdtjq6@users.noreply.github.com>
2024-10-02 14:31:57 +0900

    Update sheisgone.sh

--- sheisgone.sh
+++ sheisgone.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# 랜덤 칭찬 배열
-COMPLIMENTS=(
-    "당신은 정말 대단해요!"
-    "오늘 너무 멋져 보이네요!"
-    "코드를 작성하는 속도가 빛처럼 빠르네요!"
-    "당신의 지식은 끝이 없네요!"
-    "이 세상에서 가장 똑똑한 사람 중 한 명이에요!"
-    "모든 일을 완벽하게 해내는군요!"
-)
+echo "맥북이 10초 후 종료됩니다. 작업을 저장하세요!"
+for i in {10..1}; do
+  echo "$i초 후 종료됩니다..."
+  sleep 1
+done
 
-# 배열에서 무작위로 선택
-RANDOM_INDEX=$((RANDOM % ${#COMPLIMENTS[@]}))
-
-# 칭찬 출력
-echo ${COMPLIMENTS[$RANDOM_INDEX]}
-sleep 99999
+echo "시스템 종료 중..."
+sudo shutdown -h now
